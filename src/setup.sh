e9766fd1aed919f38b19f6c94587818318538a7e
NoNameWasDefined <169661348+NoNameWasDefined@users.noreply.github.com>
2026-05-31 11:50:17 +0200

    refactor: simplify build steps
    - remove generate.sh now useless
    - let src/setup.sh instead of BuildKit clone termux/termux-packages in
      /source (now a cache mount), to allow pulling latest commits after
      rebuilds
    - move MOTD modification from generate.sh in src/setup.sh: this step is
      now run inside the Docker build instead of the host

--- src/setup.sh
+++ src/setup.sh
@@ -1,18 +1,33 @@
 #!/system/bin/sh
+
 set -o errexit
 set -o nounset
 
 mkdir -p '/data/data/com.termux/cache/apt/archives/partial'
 ln --symbolic --relative --force \
-	"${PREFIX}/etc/termux/mirrors/europe/packages.termux.dev" \
-	"${PREFIX}/etc/termux/chosen_mirrors"
+	"$PREFIX/etc/termux/mirrors/europe/packages.termux.dev" \
+	"$PREFIX/etc/termux/chosen_mirrors"
 pkg upgrade --assume-yes --quiet --option=Dpkg::Options::=--force-confnew
-apt install --assume-yes --quiet gnupg jq
+apt install --assume-yes --quiet git gnupg jq
 
 ln --symbolic --relative --force \
-	"${PREFIX}/etc/termux/mirrors/europe/packages.termux.dev" \
-	"${PREFIX}/etc/termux/chosen_mirrors"
+	"$PREFIX/etc/termux/mirrors/europe/packages.termux.dev" \
+	"$PREFIX/etc/termux/chosen_mirrors"
 pkg --check-mirror upgrade
+
+sed --in-place \
+	--expression="s/%BUILD_DATE%/$(
+		date --utc
+	)/g" \
+	--expression="s/%ORIGIN_IMAGE_TAG%/$1/g" \
+	--expression="s/%ACTUAL_IMAGE_TAG%/$1/g" \
+	"$HOME/.termux/motd.sh"
+
+if [ -z "$(find /source -mindepth 1 -print0 -quit)" ]; then
+	git clone --depth=1 'https://github.com/termux/termux-packages.git' '/source'
+else
+	git -C '/source' pull
+fi
 
 cd '/source'
 './scripts/setup-termux.sh'
